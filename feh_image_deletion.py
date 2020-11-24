139211c9ca8a3cdc195a05ea56aa80b1f118bb8d
JohnleDo <kitsuneace@gmail.com>
2020-11-24 17:08:02 -0600

    Reformatted some files and finished script for handling image deletion during Feh runtime

---
+++ feh_image_deletion.py
@@ -0,0 +1,42 @@
+import argparse, os, json
+from datetime import datetime
+from pynput.keyboard import Key, Controller
+
+
+"""
+    We take a list of strings as an argument because when we will be using Feh we bind this script to a key. To do that
+    we run it as this command:
+        "feh -F --action1 "python3 /path_to_python_script/feh_image_deletion.py -d echo $PWD/%n %n"
+    When looking at this command we include a bash command and the reason for that is because we need to get the name
+    of the image being deleted so we can record it and store it into a json file.
+    
+    - args.d[0] = echo command
+    - args.d[1] = image file path
+    - args.d[2] = image file name
+"""
+
+if __name__ == '__main__':
+    keyboard = Controller()
+
+    parser = argparse.ArgumentParser(description='used to capture Feh image filenames before deletion.')
+    parser.add_argument('-d', nargs='+', help='Name of file to be deleted. Takes a list to deal with bash command.')
+    args = parser.parse_args()
+    feh_dir = args.d[1].replace(args.d[2], "")
+    print("Feh Image Filename to be deleted: " + args.d[2])
+
+    if os.path.exists(feh_dir + "removed_images.json"):
+        with open(feh_dir + "removed_images.json", "a") as file:
+            json.dump({"filename": args.d[2],
+                      "date of deletion": datetime.today().strftime('%Y-%m-%d')}, file)
+            file.write(os.linesep)
+            file.close()
+    else:
+        with open(feh_dir + "removed_images.json", "w") as file:
+            json.dump({"filename": args.d[2],
+                      "date of deletion": datetime.today().strftime('%Y-%m-%d')}, file)
+            file.write(os.linesep)
+            file.close()
+
+    os.remove(args.d[1])
+
+
--- keyboard_command.py
+++ keyboard_command.py
@@ -9,30 +9,35 @@ def action1(self):
     keyboard.press(Key.right)
     keyboard.release(Key.right)
 
+
 def action2(self):
     keyboard.press(Key.left)
     keyboard.release(Key.left)
-    
+
+
 def action3(self):
     keyboard.type("Greetings Mortals ")
+
 
 def action4(self):
     led.toggle()
 
-button1_pin = 17
-button2_pin = 27
-button3_pin = 22
-led = LED(10)
-keyboard= Controller()
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(button1_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(button2_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(button3_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(button1_pin, GPIO.BOTH, callback=action1,bouncetime=800)
-GPIO.add_event_detect(button2_pin, GPIO.BOTH, callback=action2, bouncetime=800)
-GPIO.add_event_detect(button3_pin, GPIO.BOTH, callback=action4, bouncetime=800)
+if __name__ == '__main__':
+    button1_pin = 17
+    button2_pin = 27
+    button3_pin = 22
+    led = LED(10)
+    keyboard = Controller()
 
-while True:
-    print("Waiting for button")
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(button1_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(button2_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(button3_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.add_event_detect(button1_pin, GPIO.BOTH, callback=action1, bouncetime=800)
+    GPIO.add_event_detect(button2_pin, GPIO.BOTH, callback=action2, bouncetime=800)
+    GPIO.add_event_detect(button3_pin, GPIO.BOTH, callback=action4, bouncetime=800)
+
+    while True:
+        print("Waiting for button")
 
