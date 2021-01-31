d751bcf9731d5f7406d80451ee29e085c2d474c6
unknown <kitsuneace@gmail.com>
2021-01-30 20:47:19 -0600

    Changed files around

---
+++ Scripts ran on DPF/test.py
@@ -0,0 +1,13 @@
+import pexpect
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='used to increase/decrease screen brightness via backlight')
+    parser.add_argument('-b', '--brightness',help='An integer value to set the brightness of monitor.')
+    args = parser.parse_args()
+
+    command = pexpect.spawn("sudo ddcutil setvcp 10 {}".format(args.brightness))
+    command.expect("password")
+    command.sendline("111696")
+    print(command.read())
+
--- test.py
+++
@@ -1,5 +0,0 @@
-import pexpect
-child = pexpect.spawn("sudo ddcutil setvcp 10 0")
-child.expect("password")
-child.sendline("111696")
-print(child.read())
