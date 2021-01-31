f7f079220f8789ede23b29e68b286865932cc03a
unknown <kitsuneace@gmail.com>
2021-01-30 21:02:53 -0600

    More files

--- DPF_Scripts/control_brightness.py
+++ DPF_Scripts/control_brightness.py
@@ -1,17 +1,12 @@
 import pexpect
 import argparse
+import time
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='used to increase/decrease screen brightness via backlight')
     parser.add_argument('-b', '--brightness',help='An integer value to set the brightness of monitor.')
     args = parser.parse_args()
 
-    command = pexpect.spawn("sudo ddcutil setvcp 10 {}".format(args.brightness))
-    command.expect("password")
-    command.sendline("111696")
-
     command = pexpect.spawn("sudo ddcutil getvcp 10")
     command.expect("password")
     command.sendline("111696")
-    print(command.read())
-
