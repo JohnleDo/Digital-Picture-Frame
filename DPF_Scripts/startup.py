fbefab3d52a380611d56a39776959050e28170f8
unknown <kitsuneace@gmail.com>
2021-02-06 20:30:34 -0600

    Wrote comments and changed the name of the script. Also added extra things

--- DPF_Scripts/startup.py
+++ DPF_Scripts/startup.py
@@ -1,10 +1,21 @@
+"""
+This is the script for getting everything up and running. These are the steps it goes through:
+1. Check if Raspberry Pi is fully turned on and connected. If not, it will continue to ping it till its pingable.
+2. Execute the Raspberry Pi script to allow for our buttons to work.
+3. Finally, it will start the Slideshow.
+"""
+
 import platform  
-import subprocess 
+import subprocess
+import os 
 
 # Raspberry Pi Information
 ip_address = "192.168.1.102"
 username = "pi"
 password = "raspberry"
+
+# The path to the directory holding all the images for the slideshow
+image_directory = "/home/dpf/Desktop/Slideshow Images"
 
 
 def ping(host):
@@ -23,5 +34,7 @@ def ping(host):
     return subprocess.call(command) == 0
 
 if __name__ == '__main__':
-    print(ping(ip_address))
+    while ping(ip_address) is False:
+        print("Could not reach Raspberry Pi, ")
+    os.chdir(image_directory)
     subprocess.call('`feh -F -d --slideshow-delay 10 --action1 "python3 /home/dpf/Desktop/Digital-Picture-Frame/DPF_Scripts/feh_image_deletion.py -d echo @$PWD/@%n %n"`'.replace("@", "'"))
